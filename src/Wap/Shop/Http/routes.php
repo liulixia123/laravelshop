b43601aef3e09052a33b42c92b28046fb1e58551
lixia <1518545702@qq.com>
2020-07-30 10:17:38 +0800

    rm routes.php

--- src/Wap/Shop/Http/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-Route::get('/','IndexController@index');
-Route::get('/wechat-menu', 'WechatMenuController@index');
-/*Route::get('/', function () {
-    dd(shop_asset('css/1.css'));
-});*/
-?>
